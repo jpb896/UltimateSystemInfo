12128c764efe5e7a7b6800aeea81455e73979f2f
jpb <jakub_baranski@outlook.com>
2025-09-19 20:16:01 +0100

    Add localization support

--- UltimateSystemInfo/App.xaml.cs
+++ UltimateSystemInfo/App.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
+﻿using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -15,6 +6,18 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using WinUI3Localizer;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -30,6 +33,9 @@ namespace UltimateSystemInfo
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
+        
+        public static string StringsFolderPath { get; private set; } = string.Empty;
+
         public App()
         {
             this.InitializeComponent();
@@ -39,10 +45,25 @@ namespace UltimateSystemInfo
         /// Invoked when the application is launched.
         /// </summary>
         /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        protected override async void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
+            await InitializeLocalizer();
             m_window = new MainWindow();
             m_window.Activate();
+        }
+
+        private async Task InitializeLocalizer()
+        {
+            StringsFolderPath = System.IO.Path.Combine(AppContext.BaseDirectory, "Strings");
+            StorageFolder stringsFolder = await StorageFolder.GetFolderFromPathAsync(StringsFolderPath);
+
+            ILocalizer localizer = await new LocalizerBuilder()
+                .AddStringResourcesFolderForLanguageDictionaries(StringsFolderPath)
+                .SetOptions(options =>
+                {
+                    options.DefaultLanguage = "en-US";
+                })
+                .Build();
         }
 
         private Window? m_window;
